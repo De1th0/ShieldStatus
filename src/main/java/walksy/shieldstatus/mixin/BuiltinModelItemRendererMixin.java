614d6bc0266d3b6c9425926685e25067063b5b77
Ozzer0 <ozzer0111@gmail.com>
2025-09-29 00:48:11 -0500

    I need a couple of your mods for 1.21.8, so I just decided to update them myself. You may need to regenerate the gradle folder for it to actually build, no clue.

--- src/main/java/walksy/shieldstatus/mixin/BuiltinModelItemRendererMixin.java
+++ src/main/java/walksy/shieldstatus/mixin/BuiltinModelItemRendererMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.client.render.item.model.special.ShieldModelRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.component.ComponentMap;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ModelTransformationMode;
+import net.minecraft.item.ItemDisplayContext;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,25 +20,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import walksy.shieldstatus.manager.ConfigManager;
 import walksy.shieldstatus.manager.ShieldRenderManager;
 
-
 @Mixin(ShieldModelRenderer.class)
 public class BuiltinModelItemRendererMixin {
-
 
     @Shadow
     @Final
     private ShieldEntityModel model;
 
-    @Unique ItemStack itemStack;
+    @Unique
+    private ItemStack itemStack;
 
-    @Inject(method = "getData(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/component/ComponentMap;", at = @At("HEAD"))
-    public void getData(ItemStack itemStack, CallbackInfoReturnable<ComponentMap> cir)
-    {
+    @Inject(method = "getData", at = @At("HEAD"))
+    public void getData(ItemStack itemStack, CallbackInfoReturnable<ComponentMap> cir) {
         this.itemStack = itemStack;
     }
 
-    @Inject(at = {@At("HEAD")}, method = {"render(Lnet/minecraft/component/ComponentMap;Lnet/minecraft/item/ModelTransformationMode;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IIZ)V"}, cancellable = true)
-    public void render(ComponentMap componentMap, ModelTransformationMode mode, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, boolean bl, CallbackInfo ci) {
+    @Inject(
+            at = @At("HEAD"),
+            method = "render",
+            cancellable = true
+    )
+    public void render(ComponentMap componentMap, ItemDisplayContext mode, MatrixStack matrixStack,
+                       VertexConsumerProvider vertexConsumerProvider, int i, int j, boolean bl, CallbackInfo ci) {
         if (!ConfigManager.modEnabled) return;
         ShieldRenderManager.INSTANCE.render(itemStack, componentMap, mode, matrixStack, vertexConsumerProvider, this.model, i, j, bl, ci);
     }
